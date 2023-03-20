b1cefc102d88c4bbb5196e73afcc2398e1e6b89b
ensi <66754841+ncor@users.noreply.github.com>
2023-03-20 22:11:40 +0300

    [+] user and course modules injection

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { CourseModule } from "src/course/course.module";
+import { UserModule } from "src/user/user.module";
 import { PaymentController } from "./payment.controller";
 import { Payment } from "./payment.entity";
 import { PaymentService } from "./payment.service";
@@ -9,7 +11,9 @@ import { PaymentService } from "./payment.service";
 @Module({
     imports: [
         ConfigModule,
-        TypeOrmModule.forFeature([ Payment ])
+        TypeOrmModule.forFeature([ Payment ]),
+        UserModule,
+        CourseModule
     ],
     controllers: [
         PaymentController
