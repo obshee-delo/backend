b3b6ebd08d652e994c1a267e74d3ce275b2216fd
ensi <66754841+ncor@users.noreply.github.com>
2023-03-22 04:49:14 +0300

    [+] query generator script

---
+++ scripts/queries.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import { join } from 'path';
+import { writeFileSync } from 'fs';
+
+const queriesRootDirectory = join(__dirname, '/../queries');
+
+/**
+ * Generates init.sql file.
+ */
+function generateDatabaseInitQuery(queriesRootDirectory: string) {
+    let filePath = join(queriesRootDirectory, 'init.sql');
+    let content = '';
+
+    content += `CREATE DATABASE ${process.env.DATABASE_NAME};\n`;
+    content += `ALTER ROLE ${process.env.DATABASE_USER} PASSWORD \'${process.env.DATABASE_PASSWORD}\';\n`;
+
+    writeFileSync(filePath, content);
+}
+
+/**
+ * Generates database query files for easier container deployment.
+ */
+function generateDatabaseQueries(queriesRootDirectory: string) {
+    generateDatabaseInitQuery(queriesRootDirectory);
+}
+
+generateDatabaseQueries(queriesRootDirectory);
