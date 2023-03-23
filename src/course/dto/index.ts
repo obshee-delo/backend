66cf95267c3690b1ca6d63a40d8917fa6cb48be6
ensi <66754841+ncor@users.noreply.github.com>
2023-03-23 23:55:21 +0300

    [+] findByCategory.dto.ts -> index.ts

--- src/course/dto/index.ts
+++ src/course/dto/index.ts
@@ -1,8 +1,8 @@
 import { IsNotEmpty, IsString } from "class-validator";
 
 
-export class FindByCategoryDto {
-    @IsString()
+export class CourseFindByCategoryDto {
     @IsNotEmpty()
+    @IsString()
     category: string;
 }
