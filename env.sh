befcc3e703741b9e86bc7de315e900d0dbc4a2b1
ensi <66754841+ncor@users.noreply.github.com>
2023-03-24 15:37:55 +0300

    change: improved

--- env.sh
+++ env.sh
@@ -1,24 +1,42 @@
 # Environment preparation for non-docker deployment.
 
+# System preparations:
+
 sudo -s
 
 apt update
 
-apt install node
+# Node.js environment installation:
+
+apt install nodejs
 
 apt install npm
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+source ~/.bashrc
+
+nvm install stable
+
+nvm use stable
+
+npm i -g npx @nestjs/cli typescript ts-node
+
+npm i
+
+# PostgreSQL installation:
 
 apt install postgresql postgresql-contrib
 
 systemctl start postgresql.service
 
-npm install -g npx nest typescript ts-node
+psql -f queries/init.sql
+
+# Project preparations and deployment:
 
 npm run script:dotenv
 
 npm run script:queries
-
-psql -f queries/init.sql
 
 npm run build
 
